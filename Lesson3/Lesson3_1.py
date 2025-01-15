b6bb0e95504ea2fd7c61dc548adcf0d974f5a096
Julia-ox <Kozlova.s.u@icloud.com>
2025-01-15 19:12:02 +0300

    Lesson3_1.py

--- Lesson3/Lesson3_1.py
+++ Lesson3/Lesson3_1.py
@@ -1,1 +1,27 @@
-print("привет")
+# user.py
+
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    
+    def print_first_name(self):
+        print(self.first_name)
+    
+    def print_last_name(self):
+        print(self.last_name)
+    
+    def print_full_name(self):
+        print(f"{self.first_name} {self.last_name}")
+
+  # lesson_3_task_1.py
+
+from user import User
+
+# Создаем экземпляр User
+my_user = User("Иван", "Иванов")
+
+# Вызываем методы у объекта my_user
+my_user.print_first_name()  # Печатает имя
+my_user.print_last_name()   # Печатает фамилию
+my_user.print_full_name()    # Печатает полное имя
