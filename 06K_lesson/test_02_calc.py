cff2cdc0adffcdf1f1101848b4314e1f60144513
Julia <Kozlova.s.u@icloud.com>
2025-03-11 13:10:27 +0300

    lesson_6

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,4 +1,3 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -6,34 +5,34 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Указываем путь к файлу chromedriver.exe
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def set_zoom(driver, zoom_percentage):
+    driver.execute_script(f"document.body.style.zoom = '{zoom_percentage}%';")
 
-# Открываем страницу
-driver.get('https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html')
 
-try:
-    # Ожидаем появления и вводим значение в поле задержки
-    delay_input = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, 'delay')))
-    delay_input.clear()
-    delay_input.send_keys('45')
+def test_calculator():
+    driver = webdriver.Chrome()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    set_zoom(driver, 80)
 
-    # Находим и нажимаем кнопки
-    button_7 = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'btn-outline-primary') and text()='7']")))
-    button_plus = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[value='+']")))
-    button_8 = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[value='8']")))
-    button_equals = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[value='=']")))
+    # Вводим задержку
+    delay = driver.find_element(By.CSS_SELECTOR, "input#delay")
+    delay.clear()
+    delay.send_keys("45")
 
-    button_7.click()
-    button_plus.click()
-    button_8.click()
-    button_equals.click()
+    # Выполняем расчет 7 + 8
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-    # Ожидаем появления результата и проверяем его
-    result = WebDriverWait(driver, 75).until(EC.text_to_be_present_in_element((By.ID, 'result'), '15'))
+    # Ожидаем появления результата
+    WebDriverWait(driver, 46).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+    )
 
-    assert result == True, "Результат не равен 15"
+    # Проверяем результат
+    result = driver.find_element(By.CSS_SELECTOR, ".screen").text
+    assert int(result) == 15
 
-finally:
     # Закрываем браузер
     driver.quit()
